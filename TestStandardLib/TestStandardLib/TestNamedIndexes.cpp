5061f2a9c04a79b1894865883365d3e034021f13
Andrew Paterson <andrew.ian.paterson@gmail.com>
2012-01-29 17:26:50 +0200

    AP: Test (failing) named index adding.

--- TestStandardLib/TestStandardLib/TestNamedIndexes.cpp
+++ TestStandardLib/TestStandardLib/TestNamedIndexes.cpp
@@ -1,3 +1,6 @@
+#include "CoreLib/DurableFileController.h"
+#include "StandardLib/NamedIndexes.h"
+#include "StandardLib/BaseObject.h"
 #include "TestLib/Assert.h"
 #include "TestNamedIndexes.h"
 
@@ -6,9 +9,28 @@
 //
 //
 //////////////////////////////////////////////////////////////////////////
-void TestStuff(void)
+void TestAdd(void)
 {
+	CNamedIndexes			cNamedIndexes;
+	CDurableFileController	cController;
+	CFileUtil				cFileUtil;
 
+	cFileUtil.RemoveDir("NamedIndexes1");
+	cFileUtil.MakeDir("NamedIndexes1");
+	cController.Init("NamedIndexes1", TRUE);
+	cNamedIndexes.Init(&cController, 10 MB, 4);
+
+	cNamedIndexes.Add(45LL, "Berty");
+	AssertLongLongInt(45LL, cNamedIndexes.GetIndex("Berty"));
+
+	cNamedIndexes.Add(73LL, "Alfred");
+	AssertLongLongInt(45LL, cNamedIndexes.GetIndex("Berty"));
+	AssertLongLongInt(73LL, cNamedIndexes.GetIndex("Alfred"));
+	AssertLongLongInt(INVALID_OBJECT_IDENTIFIER, cNamedIndexes.GetIndex(""));
+	AssertLongLongInt(INVALID_OBJECT_IDENTIFIER, cNamedIndexes.GetIndex("Ax"));
+	AssertLongLongInt(INVALID_OBJECT_IDENTIFIER, cNamedIndexes.GetIndex((char*)NULL));
+
+	cFileUtil.RemoveDir("NamedIndexes1");
 }
 
 
@@ -20,8 +42,7 @@ void TestNamedIndexes(void)
 {
 	BeginTests();
 
-	TestStuff();
+	TestAdd();
 
 	TestStatistics();
-
 }
