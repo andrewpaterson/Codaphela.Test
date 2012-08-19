c03580363b3afd2f424bd487c9f94ab4f7561e3d
Andrew Paterson <andrew.ian.paterson@gmail.com>
2012-08-19 11:16:22 +0200

    Begun adding multiple object sources to converters.

--- TestStandardLib/TestStandardLib/TestObjectConverter.cpp
+++ TestStandardLib/TestStandardLib/TestObjectConverter.cpp
@@ -2,6 +2,7 @@
 #include "BaseLib/MemoryFile.h"
 #include "StandardLib/Objects.h"
 #include "StandardLib/ObjectConverterText.h"
+#include "StandardLib/ObjectConverterChunked.h"
 #include "StandardLib/String.h"
 #include "TestLib/Assert.h"
 #include "TestObjectConverter.h"
@@ -28,10 +29,30 @@ void TestObjectConverterText(void)
 	pcObjectSource = cTextConverter.CreateSource(&cMemoryFile, "");
 	pcString = pcObjectSource->Convert("");
 	AssertString(szTextInFile, pcString->Text());
+	pcString->Kill();
 
 	cMemoryFile.Kill();
 	pcObjectSource->Kill();
 	cTextConverter.Kill();
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestObjectConverterDragon(void)
+{
+	CObjectConverterChunked	cChunkedConverter;
+	CObjectSource*			pcObjectSource;
+	CPointerObject			pcObject;
+
+	cChunkedConverter.Init();
+	pcObjectSource = cChunkedConverter.CreateSource(NULL, "");
+	pcObjectSource->Convert("");
+
+	pcObjectSource->Kill();
+	cChunkedConverter.Kill();
 }
 
 
@@ -45,6 +66,7 @@ void TestObjectConverter(void)
 	BeginTests();
 
 	TestObjectConverterText();
+	TestObjectConverterDragon();
 
 	TestStatistics();
 	ObjectsKill();
