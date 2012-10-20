285c69408be75ee8491df79ced77a745fe53f94a
Andrew Paterson <andrew.ian.paterson@gmail.com>
2012-10-20 13:22:05 +0200

    Test LogFile Open.

--- TestCoreLib/TestCoreLib/TestLogFile.cpp
+++ TestCoreLib/TestCoreLib/TestLogFile.cpp
@@ -1,12 +1,43 @@
 #include "BaseLib/FastFunctions.h"
 #include "BaseLib/FileUtil.h"
+#include "BaseLib/MemoryFile.h"
 #include "CoreLib/TypeConverter.h"
 #include "CoreLib/LogFile.h"
 #include "TestLib/Assert.h"
 
 
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestLogFileOpen(void)
+{
+	CLogFile*		pcLogFile;
+	CMemoryFile*	pcMemoryFile;
+	CFileBasic		cFile;
+	BOOL			bResult;
+
+	pcMemoryFile = MemoryFile();
+	pcLogFile = LogFile(pcMemoryFile);
+	AssertNotNull(pcLogFile);
+
+	cFile.Init(pcLogFile);
+
+	bResult = cFile.Open(EFM_ReadWrite_Create);
+	AssertTrue(bResult);
+
+	bResult = cFile.Close();
+	AssertTrue(bResult);
+
+	cFile.Kill();
+}
 
 
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
 void TestLogFile(void)
 {
 	BeginTests();
@@ -14,9 +45,10 @@ void TestLogFile(void)
 	FastFunctionsInit();
 	TypeConverterInit();
 
+	TestLogFileOpen();
+
 	FastFunctionsKill();
 	TypeConverterKill();
 
 	TestStatistics();
-
 }
