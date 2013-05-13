178af37853c37b303eccea3bd1635ac664dc8535
andrew.ian.paterson@gmail.com <andrew.ian.paterson@gmail.com>
2013-05-13 23:11:57 +0200

    Added more object allocator tests.

--- TestStandardLib/TestStandardLib/TestObjectAllocator.cpp
+++ TestStandardLib/TestStandardLib/TestObjectAllocator.cpp
@@ -1,26 +1,121 @@
 #include "StandardLib/Objects.h"
 #include "StandardLib/ObjectAllocator.h"
 #include "TestLib/Assert.h"
+#include "NamedObjectTestClasses.h"
 
 
-void TestObjectAllocatorOverwrite(void)
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestObjectAllocatorAddConstructors(void)
 {
-	CObjectAllocator	cAllocator;
-
-	ObjectsInit();
-
-	cAllocator.Init(&gcObjects, TRUE);
-
-	cAllocator.Add()
+	gcObjects.AddConstructor<CRoot>();
+	gcObjects.AddConstructor<CTestNamedObject>();
 }
 
 
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestObjectAllocatorSimpleAdd(void)
+{
+	CObjectAllocator			cAllocator;
+	CPointer<CTestNamedObject>	pNamed;
+	CPointer<CTestNamedObject>	pTemp;
+
+	ObjectsInit();
+
+	TestObjectAllocatorAddConstructors();
+	cAllocator.Init(&gcObjects, TRUE);
+
+	pNamed = cAllocator.Add("CTestNamedObject");
+	AssertNotNull(pNamed.Object());
+
+	AssertLongLongInt(1LL, pNamed.GetIndex());
+	AssertString("", pNamed.GetName());
+
+	pTemp = gcObjects.Get("");
+	AssertNull(pTemp.Object());
+	pTemp = gcObjects.Get(1LL);
+	AssertPointer(pNamed.Object(), pTemp.Object());
+
+	ObjectsKill();
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestObjectAllocatorNamedAdd(void)
+{
+	CObjectAllocator			cAllocator;
+	CPointer<CTestNamedObject>	pNamed1;
+	CPointer<CTestNamedObject>	pNamed2;
+	CPointer<CTestNamedObject>	pTemp;
+
+	ObjectsInit();
+
+	TestObjectAllocatorAddConstructors();
+	cAllocator.Init(&gcObjects, TRUE);
+
+	pNamed1 = cAllocator.Add("CTestNamedObject", "My Object Name");
+	AssertNotNull(pNamed1.Object());
+
+	AssertLongLongInt(1LL, pNamed1.GetIndex());
+	AssertString("My Object Name", pNamed1.GetName());
+
+	pTemp = gcObjects.Get("My Object Name");
+	AssertPointer(pNamed1.Object(), pTemp.Object());
+
+	pNamed2 = cAllocator.Add("CTestNamedObject", "My Object Name");
+	AssertNull(pNamed2.Object());
+
+	ObjectsKill();
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestObjectAllocatorNamedOverwrite(void)
+{
+	CObjectAllocator			cAllocator;
+	CPointer<CTestNamedObject>	pNamed1;
+	CPointer<CTestNamedObject>	pNamed2;
+	CPointer<CTestNamedObject>	pTemp;
+	long long int				oiExisting;
+
+	ObjectsInit();
+
+	TestObjectAllocatorAddConstructors();
+	cAllocator.Init(&gcObjects, TRUE);
+
+	pNamed1 = cAllocator.Add("CTestNamedObject", "My Object Name");
+	AssertLongLongInt(1LL, pNamed1.GetIndex());
+	pNamed2 = cAllocator.Add("CTestNamedObject", "My Object Name", 1LL, &oiExisting);
+	
+
+	ObjectsKill();
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
 void TestObjectAllocator(void)
 {
 	BeginTests();
 
-	TestObjectAllocatorOverwrite();
+	TestObjectAllocatorSimpleAdd();
+	TestObjectAllocatorNamedAdd();
+	TestObjectAllocatorNamedOverwrite();
 
 	TestStatistics();
 }
+
 
