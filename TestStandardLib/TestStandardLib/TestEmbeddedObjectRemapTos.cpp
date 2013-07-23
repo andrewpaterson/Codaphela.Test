b489e6de5aba6937e60e6343460697b9b16b69c9
andrew.ian.paterson@gmail.com <andrew.ian.paterson@gmail.com>
2013-07-23 23:11:25 +0200

    Added a test for remapping tos on a hollow object with embedded objects.

---
+++ TestStandardLib/TestStandardLib/TestEmbeddedObjectRemapTos.cpp
@@ -0,0 +1,88 @@
+#include "StandardLib/Objects.h"
+#include "StandardLib/ObjectAllocator.h"
+#include "StandardLib/String.h"
+#include "StandardLib/PointerContainer.h"
+#include "TestLib/Assert.h"
+
+
+class CTestObjectWithTwoEmbedded : public CNamedObject
+{
+BASE_FUNCTIONS(CTestObjectWithTwoEmbedded);
+public:
+	CString		mpString1;
+	CString		mpString2;
+
+	void Class(void)
+	{
+		Embedded(&mpString1);
+		Embedded(&mpString2);
+	}
+
+	Ptr<CTestObjectWithTwoEmbedded> Init(void) 
+	{
+		Class();
+		return this;
+	}
+
+	void KillData(void)
+	{
+	}
+
+	BOOL Save(CObjectSerialiser* pcFile)
+	{
+		return FALSE;
+	}
+
+	BOOL Load(CObjectDeserialiser* pcFile)
+	{
+		Class();
+		return FALSE;
+	}
+};
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestEmbeddedObjectRemapTosActual(void)
+{
+	CObjectAllocator	cAllocator;
+
+	ObjectsInit();
+	cAllocator.Init(&gcObjects);
+
+	Ptr<CRoot>	pRoot = ORoot();
+
+	Ptr<CNamedHollowObject>	pHollow = cAllocator.AddHollow("Name", gcObjects.GetIndexGenerator()->PopIndex(), 2);
+	AssertInt(2, pHollow.Object()->GetNumEmbedded());
+
+	Ptr<CEmbeddedObject> pEmbedded1 = pHollow.Object()->GetEmbeddedObject(0);
+	Ptr<CEmbeddedObject> pEmbedded2 = pHollow.Object()->GetEmbeddedObject(1);
+
+	Ptr<CPointerContainer>	pPointerHollow = gcObjects.Add<CPointerContainer>()->Init(pHollow);
+	Ptr<CPointerContainer>	pPointerEmbedded1 = gcObjects.Add<CPointerContainer>()->Init(pEmbedded1);
+	Ptr<CPointerContainer>	pPointerEmbedded2 = gcObjects.Add<CPointerContainer>()->Init(pEmbedded2);
+
+	pRoot->Add(pHollow);
+
+	Ptr<CTestObjectWithTwoEmbedded> pObject = gcObjects.Add<CTestObjectWithTwoEmbedded>("Name2")->Init();
+
+
+	cAllocator.Kill();
+	ObjectsKill();
+}
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestEmbeddedObjectRemapTos(void)
+{
+	BeginTests();
+
+	TestEmbeddedObjectRemapTosActual();
+
+	TestStatistics();
+}
+
