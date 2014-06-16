dec87a6734e38db53293b7ee3eac6d3a00b47871
Andrew Paterson <andrew.ian.paterson@gmail.com>
2014-06-16 13:35:03 +0200

    Added test TestEmbeddedStackPointersEmbeddedDistPassThruPointer.

--- TestStandardLib/TestStandardLib/TestEmbeddedStackPointers.cpp
+++ TestStandardLib/TestStandardLib/TestEmbeddedStackPointers.cpp
@@ -9,12 +9,13 @@
 //
 //
 //////////////////////////////////////////////////////////////////////////
-void TestEmbeddedStackPointersEmbeddedDist(void)
+void TestEmbeddedStackPointersEmbeddedDistPassThruPointer(void)
 {
 	ObjectsInit();
 
-	CEmbeddedTest*		pTest;
+	CEmbeddedTest*		pcTest;
 	CEmbeddedComplex	cComplex;
+	Ptr<CEmbeddedTest>	pTest;
 	
 	cComplex.Class();
 	cComplex.Init();
@@ -31,7 +32,51 @@ void TestEmbeddedStackPointersEmbeddedDist(void)
 
 	AssertInt(0, cComplex.GetDistToStack());
 
+	pTest = OMalloc(CEmbeddedTest);
+	pcTest = (CEmbeddedTest*)pTest.Object();
+
+	pTest->Init();
+	cComplex.mpTest = pTest;
+
+	AssertInt(1, pcTest->GetDistToStack());
+	
+	pTest = NULL;
+	AssertInt(1, pcTest->GetDistToStack());
+
+	cComplex.Kill();
+
+	ObjectsKill();
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestEmbeddedStackPointersEmbeddedDistDirect(void)
+{
+	ObjectsInit();
+
+	CEmbeddedTest*		pTest;
+	CEmbeddedComplex	cComplex;
+
+	cComplex.Class();
+	cComplex.Init();
+	cComplex.mai[0] = 1234;
+	cComplex.mai[1] = 7890;
+
+	AssertInt(0, cComplex.GetDistToStack());
+
+	Ptr<CEmbeddedComplex> pComplex;
+	pComplex = &cComplex;
+
+	AssertInt(1234, pComplex->mai[0]);
+	AssertInt(7890, pComplex->mai[1]);
+
+	AssertInt(0, cComplex.GetDistToStack());
+
 	cComplex.mpTest = OMalloc(CEmbeddedTest)->Init();
+
 	pTest = (CEmbeddedTest*)cComplex.mpTest.Object();
 	AssertInt(1, pTest->GetDistToStack());
 
@@ -39,7 +84,6 @@ void TestEmbeddedStackPointersEmbeddedDist(void)
 
 	ObjectsKill();
 }
-
 
 
 //////////////////////////////////////////////////////////////////////////
@@ -50,7 +94,8 @@ void TestEmbeddedStackPointers(void)
 {
 	BeginTests();
 
-	TestEmbeddedStackPointersEmbeddedDist();
+	TestEmbeddedStackPointersEmbeddedDistPassThruPointer();
+	TestEmbeddedStackPointersEmbeddedDistDirect();
 
 	TestStatistics();
 }
