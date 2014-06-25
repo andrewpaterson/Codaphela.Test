d23c0d4694e720eda6e80f935429b23c5f54cab0
Andrew Paterson <andrew.ian.paterson@gmail.com>
2014-06-25 15:49:43 +0200

    Forgot to commit this.

--- TestCoreLib/TestCoreLib/TestArraySimple.cpp
+++ TestCoreLib/TestCoreLib/TestArraySimple.cpp
@@ -14,24 +14,24 @@ void TestArraySimple(void)
 	CArraySimpleChar	cac;
 
 	cac.Init("aabbc");
-	AssertString("aabbc", cac.pvArray);
+	AssertString("aabbc", cac.GetArray());
 
 	cac.BatchInsertElements(0, 3, 2, 2);
 
-	cac.pvArray[0]=cac.pvArray[1]=cac.pvArray[2]='_';
-	cac.pvArray[5]=cac.pvArray[6]=cac.pvArray[7]='_';
-	AssertString("___aa___bbc", cac.pvArray);
+	cac.GetArray()[0] = cac.GetArray()[1] = cac.GetArray()[2]='_';
+	cac.GetArray()[5] = cac.GetArray()[6] = cac.GetArray()[7]='_';
+	AssertString("___aa___bbc", cac.GetArray());
 
 	cac.InsertNumElementsAt(2, 1);
 
-	cac.pvArray[1]=cac.pvArray[2]='X';
-	AssertString("_XX__aa___bbc", cac.pvArray);
+	cac.GetArray()[1]=cac.GetArray()[2]='X';
+	AssertString("_XX__aa___bbc", cac.GetArray());
 
 	cac.Kill();
 
 	cac.Init("ABCDEFGHIJK");
 	cac.BatchRemoveElements(2, 2, 3, 3);
-	AssertString("ABEHK", cac.pvArray);
+	AssertString("ABEHK", cac.GetArray());
 
 	cac.Kill();
 
