6d90b3f811803790210a2ebd3ab690a7784a2ef0
Andrew Paterson <andrew.ian.paterson@gmail.com>
2014-06-28 21:49:41 +0200

    Added insertion tests for TreeTemplate.

--- TestBaseLib/TestBaseLib/TestTreeTemplate.cpp
+++ TestBaseLib/TestBaseLib/TestTreeTemplate.cpp
@@ -7,10 +7,78 @@ struct STestTreeTemplateItem
 {
 	int	i1;
 	int	i2;
+
+	void Init(int i1, int i2);
+	BOOL IsOkay(int i1, int i2);
 };
+void STestTreeTemplateItem::Init(int i1, int i2)
+{
+	this->i1 = i1;
+	this->i2 = i2;
+}
+BOOL STestTreeTemplateItem::IsOkay(int i1, int i2)
+{
+	return (this->i1 == i1) && (this->i2 == i2);
+}
+
 
 
 typedef CTreeTemplate<STestTreeTemplateItem> CTestTree;
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestTreeTemplateInsert(void)
+{
+	CTestTree				cTree;
+	STestTreeTemplateItem*	pcNode0;
+	STestTreeTemplateItem*	pcNode00;
+	STestTreeTemplateItem*	pcNode01;
+	STestTreeTemplateItem*	pcNode02;
+	STestTreeTemplateItem*	pcNode000;
+	STestTreeTemplateItem*	pcTest;
+
+	cTree.Init();
+	pcNode0 = cTree.InsertRoot();
+	pcNode0->Init(672, 7645);
+
+	pcNode00 = cTree.InsertOnRightOfChildren(pcNode0);
+	pcNode00->Init(720, 6);
+
+	pcNode000 = cTree.InsertOnRightOfChildren(pcNode00);
+	pcNode000->Init(325, 28274);
+
+	pcNode01 = cTree.InsertOnRightOfChildren(pcNode0);
+	pcNode01->Init(8, 12);
+
+	pcNode02 = cTree.InsertOnRightOfChildren(pcNode0);
+	pcNode02->Init(99, 7);
+
+	AssertInt(5, cTree.NumElements());
+	pcTest = cTree.GetRoot();
+	AssertPointer(pcNode0, pcTest);
+	AssertTrue(pcTest->IsOkay(672, 7645));
+	pcTest = cTree.GetUp(pcNode0);
+	AssertPointer(pcNode00, pcTest);
+	AssertTrue(pcTest->IsOkay(720, 6));
+	pcTest = cTree.GetUp(pcNode00);
+	AssertPointer(pcNode000, pcTest);
+	pcTest = cTree.GetRight(pcNode00);
+	AssertPointer(pcNode01, pcTest);
+	pcTest = cTree.GetRight(pcNode01);
+	AssertPointer(pcNode02, pcTest);
+	pcTest = cTree.GetRight(pcNode02);
+	AssertNull(pcTest);
+	AssertNull(cTree.GetUp(pcNode01));
+	AssertNull(cTree.GetUp(pcNode02));
+	AssertPointer(pcNode0, cTree.GetDown(pcNode00));
+	AssertPointer(pcNode0, cTree.GetDown(pcNode01));
+	AssertPointer(pcNode0, cTree.GetDown(pcNode02));
+
+	cTree.Kill();
+}
 
 
 //////////////////////////////////////////////////////////////////////////
@@ -83,6 +151,7 @@ void TestTreeTemplate(void)
 {
 	BeginTests();
 
+	TestTreeTemplateInsert();
 	TestTreeTemplateFileHelper();
 
 	TestStatistics();
