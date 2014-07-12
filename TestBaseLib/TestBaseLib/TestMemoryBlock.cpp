b262442f064c762e27c1dc9a48b296118f40810f
Andrew Paterson <andrew.ian.paterson@gmail.com>
2014-07-12 16:51:32 +0200

    Added global memory allocating from Memory.

--- TestBaseLib/TestBaseLib/TestMemoryBlock.cpp
+++ TestBaseLib/TestBaseLib/TestMemoryBlock.cpp
@@ -1,6 +1,7 @@
 #include "BaseLib/SystemAllocator.h"
 #include "BaseLib/FreeListAllocator.h"
 #include "BaseLib/MemoryBlock.h"
+#include "BaseLib/GlobalMemory.h"
 #include "TestLib/Assert.h"
 
 
@@ -32,12 +33,49 @@ void TestMemoryBlockAllocate(void)
 //
 //
 //////////////////////////////////////////////////////////////////////////
+void TestMemoryBlockGlobalMemory(void)
+{
+	MemoryInit();
+
+	CMemoryBlock	cBlock;
+	void*			pvMem;
+
+	AssertInt(0, gcMemory.NumElements());
+	AssertTrue(gbMemory);
+
+	cBlock.Init(&gcMemoryAllocator);
+	pvMem = cBlock.Allocate(10);
+	AssertNotNull(pvMem);
+	AssertPointer(pvMem, cBlock.GetMemory());
+	AssertInt(10, (int)cBlock.GetSize());
+	AssertInt(1, gcMemory.NumElements());
+
+	pvMem = cBlock.Allocate(20);
+	AssertNotNull(pvMem);
+	AssertPointer(pvMem, cBlock.GetMemory());
+	AssertInt(20, (int)cBlock.GetSize());
+	AssertInt(1, gcMemory.NumElements());
+
+	cBlock.Kill();
+	AssertNull(cBlock.GetMemory());
+	AssertInt(0, (int)cBlock.GetSize());
+	AssertInt(0, gcMemory.NumElements());
+
+	MemoryKill();
+	AssertFalse(gbMemory);
+}
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
 void TestMemoryBlock(void)
 {
 	BeginTests();
 	FastFunctionsInit();
 
 	TestMemoryBlockAllocate();
+	TestMemoryBlockGlobalMemory();
 
 	FastFunctionsKill();
 	TestStatistics();
