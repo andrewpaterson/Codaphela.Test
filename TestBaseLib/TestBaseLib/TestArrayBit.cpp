d1aa94ddf85ca54dcda7b3f0e3982c9779b8bd67
Andrew Paterson <andrew.ian.paterson@gmail.com>
2014-08-02 14:02:36 +0200

    Added reading and writing to ArrayBit.  ArrayBit now uses mallocators for memory allocation.

--- TestBaseLib/TestBaseLib/TestArrayBit.cpp
+++ TestBaseLib/TestBaseLib/TestArrayBit.cpp
@@ -1,4 +1,6 @@
 #include "BaseLib/ArrayBit.h"
+#include "BaseLib/MemoryFile.h"
+#include "BaseLib/GlobalMemory.h"
 #include "TestLib/Assert.h"
 
 
@@ -99,8 +101,80 @@ void TestArrayBitAdd(void)
 //
 //
 //////////////////////////////////////////////////////////////////////////
-void TestArrayBitLoad(void)
+void TestArrayBitReadWrite(void)
 {
+	CFileBasic		cFile;
+	CArrayBit		cArrayBit;
+	CArrayBit		cArrayBitIn;
+	unsigned int	ui[3];
+	cArrayBit.Init();
+
+	cArrayBit.Add(TRUE);
+	cArrayBit.Add(FALSE);
+
+	cFile.Init(MemoryFile());
+	cFile.Open(EFM_Write_Create);
+	AssertTrue(cArrayBit.Write(&cFile));
+
+	cFile.Close();
+	cFile.Open(EFM_Read);
+	
+	cArrayBitIn.Read(&cFile);
+
+	AssertInt(2, cArrayBitIn.NumElements());
+
+	AssertTrue(cArrayBitIn.Get(0));
+	AssertFalse(cArrayBitIn.Get(1));
+	cArrayBitIn.Kill();
+
+	cArrayBit.Add(TRUE);
+	cArrayBit.Add(FALSE);
+	cArrayBit.Add(TRUE);
+	cArrayBit.Add(FALSE);
+	cArrayBit.Add(TRUE);
+	cArrayBit.Add(FALSE);
+	ui[0] = 0xffcbb78e;
+	ui[1] = 0x18011861;
+	ui[2] = 0x0082d97e;
+	cArrayBit.Add(ui, 0, 88);
+	cArrayBit.Add(TRUE);
+	cArrayBit.Add(FALSE);
+	cArrayBit.Add(TRUE);
+	cArrayBit.Add(FALSE);
+	cArrayBit.Add(TRUE);
+	cArrayBit.Add(FALSE);
+	cArrayBit.Add(TRUE);
+	cArrayBit.Add(FALSE);
+	cArrayBit.Add(TRUE);
+	AssertInt(105, cArrayBit.NumElements());
+
+	cFile.Open(EFM_Write_Create);
+	AssertTrue(cArrayBit.Write(&cFile));
+
+	cFile.Close();
+	cArrayBit.Kill();
+
+	cFile.Open(EFM_Read);
+	cArrayBitIn.Read(&cFile);
+
+	AssertInt(105, cArrayBitIn.NumElements());
+	AssertTrue(cArrayBitIn.Get(0));
+	AssertFalse(cArrayBitIn.Get(1));
+	AssertIntHex(0xcbb78e55, ((int*)cArrayBitIn.GetData())[0]);
+	AssertIntHex(0x011861ff, ((int*)cArrayBitIn.GetData())[1]);
+	AssertIntHex(0x82d97e18, ((int*)cArrayBitIn.GetData())[2]);
+	AssertTrue(cArrayBitIn.Get(96));
+	AssertFalse(cArrayBitIn.Get(97));
+	AssertTrue(cArrayBitIn.Get(98));
+	AssertFalse(cArrayBitIn.Get(99));
+	AssertTrue(cArrayBitIn.Get(100));
+	AssertFalse(cArrayBitIn.Get(101));
+	AssertTrue(cArrayBitIn.Get(102));
+	AssertFalse(cArrayBitIn.Get(103));
+	AssertTrue(cArrayBitIn.Get(104));
+
+	cArrayBitIn.Kill();
+	cFile.Close();
 }
 
 
@@ -112,11 +186,12 @@ void TestArrayBit(void)
 {
 	BeginTests();
 	FastFunctionsInit();
+	MemoryInit();
 
-	AssertInt(16, sizeof(CArrayBit));
 	TestArrayBitAdd();
-	TestArrayBitLoad();
+	TestArrayBitReadWrite();
 
+	MemoryKill();
 	FastFunctionsKill();
 	TestStatistics();
 }
