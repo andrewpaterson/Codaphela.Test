3480a96eca83da116f59eb7174361b000025ed52
Andrew Paterson <andrew.ian.paterson@gmail.com>
2015-12-31 11:46:19 +0200

    Unfinished tests are bad.

--- TestCoreLib/TestCoreLib/TestIndexTreeBlockFile.cpp
+++ TestCoreLib/TestCoreLib/TestIndexTreeBlockFile.cpp
@@ -1,4 +1,5 @@
 #include "BaseLib/FileUtil.h"
+#include "BaseLib/SystemAllocator.h"
 #include "TestLib/Assert.h"
 #include "CoreLib/IndexTreeBlockFile.h"
 
@@ -9,7 +10,29 @@
 //////////////////////////////////////////////////////////////////////////
 void TestIndexTreeBlockFileSomething(void)
 {
-	CIndexTreeBlockFile	cIndexFile;
+	CIndexTreeBlockFile		cIndexFile;
+	CDurableFileController	cDurableController;
+	CFileUtil				cFileUtil;
+	char					szWorkingDirectory[] = "Database9";
+	CChars					szDirectory;
+	CChars					szRewriteDirectory;
+
+	cFileUtil.RemoveDir(szWorkingDirectory);
+
+	szDirectory.Init(szWorkingDirectory);
+	cFileUtil.AppendToPath(&szDirectory, "primary");
+	szRewriteDirectory.Init(szWorkingDirectory);
+	cFileUtil.AppendToPath(&szRewriteDirectory, "backup");
+	cDurableController.Init(szDirectory.Text(), szRewriteDirectory.Text(), TRUE);
+
+	cIndexFile.Init(&cDurableController, &gcSystemAllocator);
+
+	cIndexFile.Kill();
+	cDurableController.Kill();
+
+	szDirectory.Kill();
+	szRewriteDirectory.Kill();
+	cFileUtil.RemoveDir(szWorkingDirectory);
 }
 
 
@@ -25,4 +48,5 @@ void TestIndexTreeBlockFile(void)
 
 	TestStatistics();
 }
+
 
