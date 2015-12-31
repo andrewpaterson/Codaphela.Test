f131b14f3bee9c470eab8386de05bbc3706ddc06
Andrew Paterson <andrew.ian.paterson@gmail.com>
2015-12-31 23:22:13 +0200

    Quickly hacked in file nodes as memory nodes.

--- TestCoreLib/TestCoreLib/TestIndexTreeNodeFile.cpp
+++ TestCoreLib/TestCoreLib/TestIndexTreeNodeFile.cpp
@@ -1,3 +1,4 @@
+#include "BaseLib/Logger.h"
 #include "BaseLib/FileUtil.h"
 #include "BaseLib/SystemAllocator.h"
 #include "TestLib/Assert.h"
@@ -56,11 +57,44 @@ void TestIndexTreeNodeFileInit(void)
 {
 	CIndexTreeNodeFile*		pcNode;
 	CIndexTreeBlockFile		cIndex;
+	CIndexTreeNodeFile		cChildNode;
+	CIndexTreeNodeFile*		pcResult;
 
 	cIndex.FakeInit();
 
 	pcNode = (CIndexTreeNodeFile*)malloc(sizeof(CIndexTreeNodeFile) + sizeof(CIndexTreeChildNode));  //Node.  No object.  One child.
 	pcNode->Init(&cIndex, NULL);
+
+	AssertTrue(pcNode->IsEmpty());
+	AssertFalse(pcNode->HasNodes());
+	AssertFalse(pcNode->HasObject());
+	AssertTrue(pcNode->ValidateNodesEmpty());
+
+	pcNode->Contain(33);
+	AssertFalse(pcNode->IsEmpty());
+	AssertTrue(pcNode->HasNodes());
+	AssertFalse(pcNode->HasObject());
+
+	gcLogger.SetBreakOnError(FALSE);
+	AssertFalse(pcNode->ValidateNodesEmpty());
+	gcLogger.SetBreakOnError(TRUE);
+
+	AssertInt(33, pcNode->GetFirstIndex());
+	AssertInt(33, pcNode->GetLastIndex());
+
+	pcNode->Set(33, &cChildNode);
+	AssertFalse(pcNode->IsEmpty());
+	AssertTrue(pcNode->HasNodes());
+	AssertFalse(pcNode->HasObject());
+	AssertInt(1, pcNode->NumInitialisedIndexes());
+	AssertTrue(pcNode->ValidateNodesEmpty());
+
+	pcResult = pcNode->Get(33);
+	AssertPointer(&cChildNode, pcResult);
+
+	AssertFalse(pcNode->ContainsIndex(32));
+	AssertTrue(pcNode->ContainsIndex(33));
+	AssertFalse(pcNode->ContainsIndex(34));
 
 	free(pcNode);
 }
