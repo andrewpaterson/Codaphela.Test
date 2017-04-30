bf6ea030d5536e647acda380a0edd7620882a0e9
Andrew Paterson <andrew.ian.paterson@gmail.com>
2017-04-30 14:54:57 +0200

    Fixed indexed files test directories.

--- TestCoreLib/TestCoreLib/TestIndexedFiles.cpp
+++ TestCoreLib/TestCoreLib/TestIndexedFiles.cpp
@@ -16,18 +16,10 @@ void TestIndexedFilesWorkingDirectory(void)
 	CDurableFileController	cController;
 	char					szDirectorty[] = "Files1";
 	char					szRewriteDirectorty[] = "_Files1";
-	CChars					szName;
-	CChars					szRewrite;
 
 	cFileUtil.RemoveDirs(szDirectorty, szRewriteDirectorty, NULL);
 
-	szName.Init();
-	szRewrite.Init();
-
-	cFileUtil.AppendToPath(&szName, szDirectorty);
-	cFileUtil.AppendToPath(&szRewrite, szRewriteDirectorty);
-
-	cController.Init(szName.Text(), szRewrite.Text());
+	cController.Init(szDirectorty, szRewriteDirectorty);
 	cIndexedFiles.Init(&cController, "DAT", "Files.IDX", "_Files.IDX");
 	cController.Begin();
 
@@ -50,22 +42,59 @@ void TestIndexedFilesInitAndKillWihtoutOpen(void)
 	CDurableFileController	cController;
 	char					szDirectorty[] = "Files2";
 	char					szRewriteDirectorty[] = "_Files2";
-	CChars					szName;
-	CChars					szRewrite;
 
 
 	cFileUtil.RemoveDirs(szDirectorty, szRewriteDirectorty, NULL);
 
-	szName.Init();
-	szRewrite.Init();
-
-	cFileUtil.AppendToPath(&szName, szDirectorty);
-	cFileUtil.AppendToPath(&szRewrite, szRewriteDirectorty);
-
-	cController.Init(szName.Text(), szRewrite.Text());
+	cController.Init(szDirectorty, szRewriteDirectorty);
 	cIndexedFiles.Init(&cController, "DAT", "Files.IDX", "_Files.IDX");
 
 	cIndexedFiles.Kill();
+	cController.Kill();
+
+	cFileUtil.RemoveDirs(szDirectorty, szRewriteDirectorty, NULL);
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestIndexedFilesWrite(void)
+{
+	CIndexedFiles			cIndexedFiles;
+	CFileUtil				cFileUtil;
+	CDurableFileController	cController;
+	char					szDirectorty[] = "Files3";
+	char					szRewriteDirectorty[] = "_Files3";
+	CIndexedDataDescriptor	cDescriptor1;
+	CIndexedDataDescriptor	cDescriptor2;
+	char					szData1[] = "For the single case of just one bed of a certain type, you use bed directly with a text.";
+	char					szData2[] = "Text's trusty Rights and Export Coordinator, Khadija Caffoor, went to the London Book Fair in March.";
+	int						iLen1;
+	int						iLen2;
+
+	iLen1 = strlen(szData1);
+	iLen2 = strlen(szData2);
+
+	cFileUtil.RemoveDirs(szDirectorty, szRewriteDirectorty, NULL);
+
+	cController.Init(szDirectorty, szRewriteDirectorty);
+	cController.Begin();
+	cIndexedFiles.Init(&cController, "DAT", "Files.IDX", "_Files.IDX");
+	cController.End();
+
+	cController.Begin();
+	cDescriptor1.Init(3LL, iLen1);
+	cIndexedFiles.Write(&cDescriptor1, szData1);
+
+	cDescriptor2.Init(5LL, iLen2);
+	cIndexedFiles.Write(&cDescriptor2, szData2);
+	cController.End();
+
+	cController.Begin();
+	cIndexedFiles.Kill();
+	cController.End();
 	cController.Kill();
 
 	cFileUtil.RemoveDirs(szDirectorty, szRewriteDirectorty, NULL);
@@ -84,6 +113,7 @@ void TestIndexedFiles(void)
 
 	TestIndexedFilesInitAndKillWihtoutOpen();
 	TestIndexedFilesWorkingDirectory();
+	TestIndexedFilesWrite();
 
 	TestStatistics();
 	FastFunctionsKill();
