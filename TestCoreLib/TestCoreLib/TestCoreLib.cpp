f63448c8a0a70fac97bbc16deb01f255ae33ec3a
Andrew Paterson <andrew.ian.paterson@gmail.com>
2018-04-01 09:44:28 +0200

    Renamed IndexTreeIterator to SIndexTreeMemoryIterator

--- TestCoreLib/TestCoreLib/TestCoreLib.cpp
+++ TestCoreLib/TestCoreLib/TestCoreLib.cpp
@@ -52,7 +52,7 @@ int main(void)
 	//TestNumber();
 	//TestIndexedFile();
 	//TestIndexedFiles();
-	TestIndexTreeMemory();
+	//TestIndexTreeMemory();
 	TestIndexTreeFile();
 	TestIndexTreeAccess();
 	TestIndexTreeWriter();
