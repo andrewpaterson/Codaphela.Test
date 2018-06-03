93d1eebce939c40d33cdd768c92ca6a7eea14cd7
Andrew Paterson <andrew.ian.paterson@gmail.com>
2018-06-03 15:16:05 +0200

    Use size of allocated memory from allocator when available.

--- TestBaseLib/TestBaseLib/TestCountingAllocator.cpp
+++ TestBaseLib/TestBaseLib/TestCountingAllocator.cpp
@@ -1,5 +1,7 @@
 #include "BaseLib/MemoryAllocator.h"
 #include "BaseLib/CountingAllocator.h"
+#include "BaseLib/ScratchPadAllocator.h"
+#include "BaseLib/GlobalMemory.h"
 #include "TestLib/Assert.h"
 
 
@@ -7,7 +9,49 @@
 //
 //
 //////////////////////////////////////////////////////////////////////////
-void TestCountingAllocatorSize(void)
+void TestCountingAllocatorSizeSystemAllocator(void)
+{
+	CCountingAllocator	cCounting;
+	void*				pv1;
+	void*				pv2;
+	void*				pv3;
+	void*				pv4;
+
+	cCounting.Init(&gcSystemAllocator);
+	AssertInt(sizeof(SCountingMemoryAllocation), cCounting.SizeOffset());
+
+	AssertInt(0, cCounting.AllocatedSize());
+
+	pv1 = cCounting.Malloc(100);
+	AssertInt(100, cCounting.AllocatedSize());
+
+	pv2 = cCounting.Malloc(200);
+	AssertInt(300, cCounting.AllocatedSize());
+
+	pv3 = cCounting.Malloc(50);
+	AssertInt(350, cCounting.AllocatedSize());
+
+	pv4 = cCounting.Realloc(pv3, 1000);
+	AssertInt(1300, cCounting.AllocatedSize());
+
+	cCounting.Free(pv1);
+	AssertInt(1200, cCounting.AllocatedSize());
+
+	cCounting.Free(pv2);
+	AssertInt(1000, cCounting.AllocatedSize());
+
+	cCounting.Free(pv4);
+	AssertInt(0, cCounting.AllocatedSize());
+
+	cCounting.Kill();
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestCountingAllocatorSizeMemoryAllocator(void)
 {
 	CCountingAllocator	cCounting;
 	CMemoryAllocator	cMemory;
@@ -50,12 +94,103 @@ void TestCountingAllocatorSize(void)
 //
 //
 //////////////////////////////////////////////////////////////////////////
+void TestCountingAllocatorSizeScratchPadAllocator(void)
+{
+	CCountingAllocator		cCounting;
+	CScratchPadAllocator	cScratchPad;
+	void*					pv1;
+	void*					pv2;
+	void*					pv3;
+	void*					pv4;
+
+	cScratchPad.Init();
+	cCounting.Init(&cScratchPad);
+	AssertInt(0, cCounting.AllocatedSize());
+
+	pv1 = cCounting.Malloc(100);
+	AssertInt(100, cCounting.AllocatedSize());
+
+	pv2 = cCounting.Malloc(200);
+	AssertInt(300, cCounting.AllocatedSize());
+
+	pv3 = cCounting.Malloc(50);
+	AssertInt(350, cCounting.AllocatedSize());
+
+	pv4 = cCounting.Realloc(pv3, 1000);
+	AssertInt(1300, cCounting.AllocatedSize());
+
+	cCounting.Free(pv1);
+	AssertInt(1200, cCounting.AllocatedSize());
+
+	cCounting.Free(pv2);
+	AssertInt(1000, cCounting.AllocatedSize());
+
+	cCounting.Free(pv4);
+	AssertInt(0, cCounting.AllocatedSize());
+
+	cCounting.Kill();
+	cScratchPad.Kill();
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestCountingAllocatorSizeGlobalMemoryAllocator(void)
+{
+	CCountingAllocator		cCounting;
+	void*					pv1;
+	void*					pv2;
+	void*					pv3;
+	void*					pv4;
+
+	MemoryInit();
+
+	cCounting.Init(&gcMemoryAllocator);
+	AssertInt(0, cCounting.AllocatedSize());
+
+	pv1 = cCounting.Malloc(100);
+	AssertInt(100, cCounting.AllocatedSize());
+
+	pv2 = cCounting.Malloc(200);
+	AssertInt(300, cCounting.AllocatedSize());
+
+	pv3 = cCounting.Malloc(50);
+	AssertInt(350, cCounting.AllocatedSize());
+
+	pv4 = cCounting.Realloc(pv3, 1000);
+	AssertInt(1300, cCounting.AllocatedSize());
+
+	cCounting.Free(pv1);
+	AssertInt(1200, cCounting.AllocatedSize());
+
+	cCounting.Free(pv2);
+	AssertInt(1000, cCounting.AllocatedSize());
+
+	cCounting.Free(pv4);
+	AssertInt(0, cCounting.AllocatedSize());
+
+	cCounting.Kill();
+	
+
+	MemoryKill();
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
 void TestCountingAllocator(void)
 {
 	BeginTests();
 	FastFunctionsInit();
 
-	TestCountingAllocatorSize();
+	TestCountingAllocatorSizeSystemAllocator();
+	TestCountingAllocatorSizeMemoryAllocator();
+	TestCountingAllocatorSizeScratchPadAllocator();
+	TestCountingAllocatorSizeGlobalMemoryAllocator();
 
 	FastFunctionsKill();
 	TestStatistics();
