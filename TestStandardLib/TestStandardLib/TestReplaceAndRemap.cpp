7b53648a6f4c906022650cff5a5810097e41b071
Andrew Paterson <andrew.ian.paterson@gmail.com>
2021-06-26 21:39:31 +0200

    Test Object Replace.

--- TestStandardLib/TestStandardLib/TestReplaceAndRemap.cpp
+++ TestStandardLib/TestStandardLib/TestReplaceAndRemap.cpp
@@ -27,16 +27,16 @@ void TestReplaceAndRemapAddConstructors(void)
 //////////////////////////////////////////////////////////////////////////
 void TestObjectReplaceNoPointers(void)
 {
-	Ptr<CTestObject>						pObject1;
-	Ptr<CTestObject>						pObject2;
-	Ptr<CTestObject>						pObject3;
-	Ptr<CTestObject>						pObject4;
-	STestObjectFreedNotifier				sFreeNotifider1;
-	STestObjectFreedNotifier				sFreeNotifider2;
-	STestObjectFreedNotifier				sFreeNotifider3;
-	Ptr<CRoot>								pRoot;
-	OIndex									oi1;
-	OIndex									oi3;
+	Ptr<CTestObject>			pObject1;
+	Ptr<CTestObject>			pObject2;
+	Ptr<CTestObject>			pObject3;
+	Ptr<CTestObject>			pObject4;
+	STestObjectFreedNotifier	sFreeNotifider1;
+	STestObjectFreedNotifier	sFreeNotifider2;
+	STestObjectFreedNotifier	sFreeNotifider3;
+	Ptr<CRoot>					pRoot;
+	OIndex						oi1;
+	OIndex						oi3;
 
 	ObjectsInit();
 	TestReplaceAndRemapAddConstructors();
@@ -67,6 +67,132 @@ void TestObjectReplaceNoPointers(void)
 	AssertPointer(&pObject3, &pObject4);
 	AssertInt(4, gcUnknowns.NumElements());
 
+	pObject4 = gcObjects.Get("Pavel");
+	AssertNull(&pObject4);
+	pObject4 = gcObjects.Get("Ned");
+	AssertPointer(&pObject3, &pObject4);
+	pObject4 = gcObjects.Get(oi1);
+	AssertNull(&pObject4);
+	pObject4 = gcObjects.Get(oi3);
+	AssertPointer(&pObject3, &pObject4);
+
+	ObjectsFlush();
+	ObjectsKill();
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestObjectReplaceWithPointers(void)
+{
+	Ptr<CTestSaveableObject2>	pObject1;
+	Ptr<CTestSaveableObject1>	pObject2;
+	Ptr<CTestObject>			pObject3;
+	Ptr<CTestObject>			pObject4;
+	Ptr<CTestEmbeddedStrings>	pObject5;
+	Ptr<>						pTest;
+	STestObjectFreedNotifier	sFreeNotifider3;
+	STestObjectFreedNotifier	sFreeNotifider4;
+	Ptr<CRoot>					pRoot;
+	OIndex						oi1;
+	OIndex						oi2;
+	OIndex						oi3;
+	OIndex						oi4;
+	OIndex						oi5;
+
+	ObjectsInit();
+	TestReplaceAndRemapAddConstructors();
+	AssertInt(0, gcUnknowns.NumElements());
+
+	pRoot = ORoot();
+	pObject1 = ONMalloc<CTestSaveableObject2>("Pavel", "Pavel");
+	pObject2 = ONMalloc<CTestSaveableObject1>("Meninski");
+	pObject3 = ONMalloc<CTestObject>("Ned", &sFreeNotifider3);
+	pObject4 = ONMalloc<CTestObject>("Jeff", &sFreeNotifider4);
+	pObject5 = OMalloc<CTestEmbeddedStrings>();
+	pRoot->Add(pObject1);
+	pRoot->Add(pObject4);
+	pObject4->mpObject = pObject1;
+	pObject1->mp1 = pObject2;
+	pObject1->mp2 = pObject3;
+	pObject3->mpObject = pObject2;
+
+	//  pObject2(3) <--- pObject3(3)
+	//   "Meninski"      "Ned"
+	//        ^          ^
+	//         \        /
+	//          \      /
+	//           \    / 
+	//            \  /
+	//         pObject1(2) <--- pObject4(2)  
+	//             "Pavel"      "Jeff"
+	//                ^          ^
+	//                 \        /
+	//                  \      /
+	//                   \    / 
+	//                    \  /
+	//                    ...
+	//                   Root(0)
+
+	oi1 = pObject1->GetIndex();
+	oi2 = pObject2->GetIndex();
+	oi3 = pObject3->GetIndex();
+	oi4 = pObject4->GetIndex();
+	oi5 = pObject5->GetIndex();
+	pObject2 = NULL;
+	pObject3 = NULL;
+	pRoot = NULL;
+	AssertLongLongInt(7, gcObjects.NumMemoryIndexes());
+	AssertLongLongInt(5, gcObjects.NumMemoryNames());
+	AssertInt(7, gcUnknowns.NumElements());
+	AssertFalse(oi1 == oi2);
+	AssertFalse(oi1 == oi3);
+	AssertFalse(oi1 == oi4);
+	AssertFalse(oi1 == oi5);
+	AssertFalse(sFreeNotifider3.bFreed);
+	AssertFalse(sFreeNotifider4.bFreed);
+
+	gcObjects.ReplaceBaseObject(&pObject1, &pObject5);
+
+	//         pObject5(2) <--- pObject4(2)  
+	//               ^          "Jeff"
+	//                \          ^
+	//                 \        /
+	//                  \      /
+	//                   \    / 
+	//                    \  /
+	//                    ...
+	//                   Root(0)
+
+	AssertLongLongInt(4, gcObjects.NumMemoryIndexes());
+	AssertLongLongInt(2, gcObjects.NumMemoryNames());
+	AssertInt(4, gcUnknowns.NumElements());
+	AssertTrue(sFreeNotifider3.bFreed);
+	AssertFalse(sFreeNotifider4.bFreed);
+
+	pTest = gcObjects.Get("Pavel");
+	AssertNull(&pTest);
+	pTest = gcObjects.Get("Meninski");
+	AssertNull(&pTest);
+	pTest = gcObjects.Get("Ned");
+	AssertNull(&pTest);
+	pTest = gcObjects.Get("Jeff");
+	AssertPointer(&pObject4, &pTest);
+	pTest = gcObjects.Get(oi1);
+	AssertNull(&pTest);
+	pTest = gcObjects.Get(oi2);
+	AssertNull(&pTest);
+	pTest = gcObjects.Get(oi3);
+	AssertNull(&pTest);
+	pTest = gcObjects.Get(oi4);
+	AssertPointer(&pObject4, &pTest);
+	pTest = gcObjects.Get(oi5);
+	AssertPointer(&pObject5, &pTest);
+	AssertInt(2, pObject5.GetDistToRoot());
+	AssertPointer(&pObject5, &pObject1);
+
 	ObjectsFlush();
 	ObjectsKill();
 }
@@ -84,6 +210,7 @@ void TestReplaceAndRemap(void)
 	DataIOInit();
 
 	TestObjectReplaceNoPointers();
+	TestObjectReplaceWithPointers();
 	//TestObjectRemap();
 	//TestObjectRename();
 
