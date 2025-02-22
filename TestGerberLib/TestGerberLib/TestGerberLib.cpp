8cafcd313bd9823ab49415cc58ba8c08ae5d0fcd
Andrew Paterson <andrew.ian.paterson@gmail.com>
2025-02-22 21:48:35 +0200

    Added a Gerber Test project.

---
+++ TestGerberLib/TestGerberLib/TestGerberLib.cpp
@@ -0,0 +1,33 @@
+// TestSupportLib.cpp : Defines the entry point for the console application.
+//
+
+#include "BaseLib/FastFunctions.h"
+#include "BaseLib/FileUtil.h"
+#include "BaseLib/TypeNames.h"
+#include "BaseLib/TypeConverter.h"
+#include "BaseLib/GlobalMemory.h"
+#include "StandardLib/Unknowns.h"
+#include "TestLib/Assert.h"
+
+int __cdecl main(void)
+{
+	CFileUtil		cFileUtil;
+
+	InitTotalStatistics();
+
+	FastFunctionsInit();
+	TypesInit();
+	TypeConverterInit();
+	MemoryInit();
+	UnknownsInit();
+
+	
+
+	UnknownsKill();
+	MemoryKill();
+	TypeConverterKill();
+	TypesKill();
+	FastFunctionsKill();
+	return TestTotalStatistics();
+}
+
