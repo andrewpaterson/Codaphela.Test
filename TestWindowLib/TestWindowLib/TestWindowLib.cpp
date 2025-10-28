1cfe14a3cee8ad24bff9a999aed5b8350a62d52b
Andrew Paterson <andrew.ian.paterson@gmail.com>
2025-10-28 20:23:28 +0200

    Added TestWindowLib basic framework.

---
+++ TestWindowLib/TestWindowLib/TestWindowLib.cpp
@@ -0,0 +1,47 @@
+// TestSupportLib.cpp : Defines the entry point for the console application.
+//
+
+#include "BaseLib/FastFunctions.h"
+#include "BaseLib/FileUtil.h"
+#include "BaseLib/TypeNames.h"
+#include "BaseLib/TypeConverter.h"
+#include "BaseLib/GlobalMemory.h"
+#include "BaseLib/GlobalDataTypesIO.h"
+#include "TestLib/Assert.h"
+
+
+void TestRefWindow(void);
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+int __cdecl main(void)
+{
+	CFileUtil		cFileUtil;
+
+	InitTotalStatistics();
+
+	FastFunctionsInit();
+	TypesInit();
+	TypeConverterInit();
+	MemoryInit();
+	DataIOInit();
+
+	cFileUtil.MakeDir("Output");
+	
+
+	TestRefWindow();
+
+
+	cFileUtil.RemoveDir("Output");
+
+	DataIOKill();
+	MemoryKill();
+	TypeConverterKill();
+	TypesKill();
+	FastFunctionsKill();
+	return TestTotalStatistics();
+}
+
