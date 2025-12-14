696d06b5d66748debb8445c4aa09b862e82081e3
Andrew Paterson <andrew.ian.paterson@gmail.com>
2025-12-14 20:49:54 +0200

    More style in FlowContainers.  Discovered Objects bug.

--- TestWindowLib/TestWindowLib/TestRefWindow.cpp
+++ TestWindowLib/TestWindowLib/TestRefWindow.cpp
@@ -1,5 +1,6 @@
 #include "BaseLib/GlobalMemory.h"
 #include "BaseLib/FileUtil.h"
+#include "BaseLib/StdRandom.h"
 #include "WindowLib/Window.h"
 #include "WindowLib/FlowContainer.h"
 #include "SupportLib/ColourARGB32.h"
@@ -128,11 +129,11 @@ void TestRefWindowCanvasBorder(void)
 //
 //
 //////////////////////////////////////////////////////////////////////////
-void TestRefWindowFlowContainer(void)
+void TestRefWindowFlowContainerBasic(void)
 {
 	CWinRefWindowFactory	cNativeFactory;
 	CFileUtil				cFileUtil;
-	char					szDirectory[] = "Output" _FS_ "FlowContainer";
+	char					szDirectory[] = "Output" _FS_ "FlowContainerBasic";
 
 	cFileUtil.RemoveDir(szDirectory);
 	cFileUtil.MakeDir(szDirectory);
@@ -154,7 +155,7 @@ void TestRefWindowFlowContainer(void)
 		CFlowContainer			cFlow;
 		SContainerBounds		sBounds;
 
-		cNativeFactory.Init(&gcMemoryAllocator, 96, 64, szDirectory);
+		cNativeFactory.Init(&gcMemoryAllocator, 96, 30, szDirectory);
 
 		cTick.Init(&cData, 1);
 		cTestWindow.Init("Reference Test Window", &cNativeFactory, &cTick, NULL);
@@ -219,13 +220,131 @@ void TestRefWindowFlowContainer(void)
 //
 //
 //////////////////////////////////////////////////////////////////////////
+void TestRefWindowFlowContainerRightCenteredContinue(void)
+{
+	CWinRefWindowFactory	cNativeFactory;
+	CFileUtil				cFileUtil;
+	char					szDirectory[] = "Output" _FS_ "FlowContainer2";
+
+	cFileUtil.RemoveDir(szDirectory);
+	cFileUtil.MakeDir(szDirectory);
+
+	{
+		Ptr<CWindow>							pTestWindow;
+		CArrayChars								aszFiles;
+		size									i;
+		CChars*									pszFilename;
+		CChars									szExpectedFilename;
+		Ptr<CCanvas>							pCanvas;
+		Ptr<CDrawCanvasBorder>					pDraw;
+		CTickTestRefWindow						cTick;
+		SDataTestRefWindow						cData;
+		Ptr<CFlowContainer>						pFlow;
+		SContainerBounds						sBounds;
+		ARGB32									sColour;
+		CRandom									cRandom;
+		CPointer								pNull;
+		CArrayTemplateEmbeddedBaseObjectPtr		apcFroms;
+		size									uiNumHeaps;
+		CBaseObject*							pcObject;
+
+		cNativeFactory.Init(&gcMemoryAllocator, 96, 24, szDirectory);
+
+		cTick.Init(&cData, 1);
+		pTestWindow = OMalloc<CWindow>("Reference Test Window", &cNativeFactory, &cTick, pNull);
+		AssertSize(4, pTestWindow.NumHeapFroms());
+		apcFroms.Init();
+		pTestWindow->GetHeapFroms(&apcFroms);
+		pcObject = *apcFroms.Get(0);
+		pcObject = *apcFroms.Get(1);
+		pcObject = *apcFroms.Get(2);
+		pcObject = *apcFroms.Get(3);
+		apcFroms.Kill();
+
+		pFlow = OMalloc<CFlowContainer>(pTestWindow);
+		pFlow->SetFlowStyle(CSD_Right, CSW_Continue, CSV_Center);
+		pTestWindow->SetContainer(pFlow);
+		AssertSize(3, gcObjects.NumMemoryIndexes());
+
+		cRandom.Init(6785);
+		for (i = 0; i < 10; i++)
+		{
+			sColour = Set32BitColour(cRandom.Next(0, 255) / 255.0f, cRandom.Next(0, 255) / 255.0f, cRandom.Next(0, 255) / 255.0f);
+			pDraw = OMalloc<CDrawCanvasBorder>(sColour);
+
+			pCanvas = OMalloc<CCanvas>(pTestWindow, CF_R8G8B8, pDraw);
+			pFlow->AddComponent(pCanvas);
+
+			if (i % 2 == 0)
+			{
+				pCanvas->SetDesiredSize(5 + i, 5 + i);
+			}
+			else
+			{
+				pCanvas->SetDesiredSize(5 + (10 - i), 5 + (10 - i));
+			}
+			pCanvas = NULL;
+			pDraw = NULL;
+
+			AssertSize(3 + (i + 1) * 2, gcObjects.NumMemoryIndexes());
+			AssertSize(15, pTestWindow.NumHeapFroms());
+		}
+
+		AssertSize(10, pFlow->NumComponents());
+		pFlow = NULL;
+
+		pTestWindow->Show();
+		AssertSize(34, gcObjects.NumMemoryIndexes());
+
+		AssertSize(15, pTestWindow.NumHeapFroms());
+		apcFroms.Init();
+		pTestWindow->GetHeapFroms(&apcFroms);
+		uiNumHeaps = apcFroms.NumElements();
+		for (i = 0; i < uiNumHeaps; i++)
+		{
+			pcObject = *apcFroms.Get(i);
+		}
+		apcFroms.Kill();
+		AssertSize(1, pTestWindow->NumStackFroms());
+
+		pTestWindow = NULL;
+		AssertSize(0, gcObjects.NumMemoryIndexes());
+
+		aszFiles.Init();
+		cFileUtil.FindAllFiles(szDirectory, &aszFiles, false, false);
+		AssertSize(1, aszFiles.NumElements());
+
+		for (i = 1; i < aszFiles.NumElements(); i++)
+		{
+			pszFilename = aszFiles.Get(i);
+			szExpectedFilename.Init(pszFilename);
+			szExpectedFilename.Replace("Output", "Input");
+
+			AssertFile(szExpectedFilename.Text(), pszFilename->Text());
+
+			szExpectedFilename.Kill();
+		}
+
+		aszFiles.Kill();
+	}
+
+	cNativeFactory.Kill();
+	cFileUtil.RemoveDir(szDirectory);
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
 void TestRefWindow(void)
 {
 	BeginTests();
 
-	TestRefWindowCreation();
-	TestRefWindowCanvasBorder();
-	TestRefWindowFlowContainer();
+	//TestRefWindowCreation();
+	//TestRefWindowCanvasBorder();
+	//TestRefWindowFlowContainerBasic();
+	TestRefWindowFlowContainerRightCenteredContinue();
 
 	TestStatistics();
 }
