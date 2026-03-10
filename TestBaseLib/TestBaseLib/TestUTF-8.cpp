9f2c4b3adfa36b978ba14bfd46ea06603f2ccbda
Andrew Paterson <andrew.ian.paterson@gmail.com>
2026-03-10 11:33:20 +0200

    UTF-8 parsing.

--- TestBaseLib/TestBaseLib/TestUTF-8.cpp
+++ TestBaseLib/TestBaseLib/TestUTF-8.cpp
@@ -125,6 +125,9 @@ void TestUTF8Example3(void)
 	CTextFile	cTextFile;
 	CChars		sz;
 	uint32		c32;
+	size		uiLength;
+	size		uiPos;
+	uint8		auiBuffer[0x13];
 
 	cTextFile.Init();
 	cTextFile.Read("Input" _FS_ "UTF8" _FS_ "Example3.txt");
@@ -132,24 +135,145 @@ void TestUTF8Example3(void)
 	cTextFile.Kill();
 
 	cUTF8.Init(&sz);
-
 	c32 = cUTF8.GetUint32();
-	AssertInt((uint32)0x1F469, c32);
+	AssertIntHex((uint32)0x0041, c32);
 	c32 = cUTF8.GetUint32();
-	AssertInt((uint32)0x200D, c32);
+	AssertIntHex((uint32)0x1F469, c32);
 	c32 = cUTF8.GetUint32();
-	AssertInt((uint32)0x1F3FB, c32);
+	AssertIntHex((uint32)0x200D, c32);
 	c32 = cUTF8.GetUint32();
-	AssertInt((uint32)0x200D, c32);
+	AssertIntHex((uint32)0x1F3FB, c32);
 	c32 = cUTF8.GetUint32();
-	AssertInt((uint32)0x1F9B0, c32);
+	AssertIntHex((uint32)0x200D, c32);
 	c32 = cUTF8.GetUint32();
-	AssertInt((uint32)0, c32);
-
+	AssertIntHex((uint32)0x1F9B0, c32);
+	c32 = cUTF8.GetUint32();
+	AssertIntHex((uint32)0, c32);
 	cUTF8.Kill();
+
+	cUTF8.Init(&sz);
+	uiPos = cUTF8.GetPosition();
+	uiLength = cUTF8.GetLength();
+	AssertSize(0, uiPos);
+	AssertSize(1, uiLength);
+	uiPos = cUTF8.GetPosition();
+	AssertSize(0, uiPos);
+
+	c32 = cUTF8.GetUint32();
+	AssertIntHex((uint32)0x0041, c32);
+
+	uiPos = cUTF8.GetPosition();
+	uiLength = cUTF8.GetLength();
+	AssertSize(1, uiPos);
+	AssertSize(0x12, uiLength);
+	c32 = cUTF8.GetUint32();
+	c32 = cUTF8.GetUint32();
+	c32 = cUTF8.GetUint32();
+	c32 = cUTF8.GetUint32();
+	c32 = cUTF8.GetUint32();
+	AssertIntHex((uint32)0x1F9B0, c32);
+
+	uiPos = cUTF8.GetPosition();
+	uiLength = cUTF8.GetLength();
+	AssertSize(0x13, uiPos);
+	AssertSize(0, uiLength);
+
+	c32 = cUTF8.GetUint32();
+	AssertIntHex((uint32)0, c32);
+	cUTF8.Kill();
+
+	cUTF8.Init(&sz);
+	uiPos = cUTF8.GetPosition();
+	uiLength = cUTF8.GetLength();
+	AssertSize(0, uiPos);
+	AssertSize(1, uiLength);
+	memset(auiBuffer, 0, 0x13);
+	uiLength = cUTF8.GetMulti(auiBuffer, 0x12);
+	AssertSize(1, uiLength);
+	AssertString("A", (char*)auiBuffer);
+
+	uiPos = cUTF8.GetPosition();
+	uiLength = cUTF8.GetLength();
+	AssertSize(1, uiPos);
+	AssertSize(0x12, uiLength);
+	memset(auiBuffer, 0, 0x13);
+	uiLength = cUTF8.GetMulti(auiBuffer, 0x12);
+	AssertSize(0x12, uiLength);
+	AssertString("👩‍🏻‍🦰", (char*)auiBuffer);
+
+	uiPos = cUTF8.GetPosition();
+	uiLength = cUTF8.GetLength();
+	AssertSize(0x13, uiPos);
+	AssertSize(0, uiLength);
+	memset(auiBuffer, 0, 0x13);
+	uiLength = cUTF8.GetMulti(auiBuffer, 0x12);
+	AssertSize(0, uiLength);
+	AssertString("", (char*)auiBuffer);
+	cUTF8.Kill();
+
 	sz.Kill();
 }
-//U+1F469 U+200D U+1F3FB U+200D U+1F9B0
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestUTF8Example4(void)
+{
+	CUTF8		cUTF8;
+	CTextFile	cTextFile;
+	CChars		sz;
+	size		uiLength;
+	size		uiPos;
+	uint8		auiBuffer[0x13];
+
+	cTextFile.Init();
+	cTextFile.Read("Input" _FS_ "UTF8" _FS_ "Example4.txt");
+	sz.Init(cTextFile.Text());
+	cTextFile.Kill();
+
+	cUTF8.Init(&sz);
+	uiPos = cUTF8.GetPosition();
+	uiLength = cUTF8.GetLength();
+	AssertSize(0, uiPos);
+	AssertSize(2, uiLength);
+	memset(auiBuffer, 0, 0x13);
+	uiLength = cUTF8.GetMulti(auiBuffer, 0x12);
+	AssertSize(2, uiLength);
+	AssertString("ł", (char*)auiBuffer);
+
+	uiPos = cUTF8.GetPosition();
+	uiLength = cUTF8.GetLength();
+	AssertSize(2, uiPos);
+	AssertSize(0x12, uiLength);
+	memset(auiBuffer, 0, 0x13);
+	uiLength = cUTF8.GetMulti(auiBuffer, 0x12);
+	AssertSize(0x12, uiLength);
+	AssertString("👩‍🏻‍🦰", (char*)auiBuffer);
+
+	uiPos = cUTF8.GetPosition();
+	uiLength = cUTF8.GetLength();
+	AssertSize(0x14, uiPos);
+	AssertSize(2, uiLength);
+	memset(auiBuffer, 0, 0x13);
+	uiLength = cUTF8.GetMulti(auiBuffer, 0x12);
+	AssertSize(2, uiLength);
+	AssertString("ł", (char*)auiBuffer);
+
+	uiPos = cUTF8.GetPosition();
+	uiLength = cUTF8.GetLength();
+	AssertSize(0x16, uiPos);
+	AssertSize(0, uiLength);
+	memset(auiBuffer, 0, 0x13);
+	uiLength = cUTF8.GetMulti(auiBuffer, 0x12);
+	AssertSize(0, uiLength);
+	AssertString((char*)auiBuffer, "");
+	cUTF8.Kill();
+
+	sz.Kill();
+}
+
 
 //////////////////////////////////////////////////////////////////////////
 //
@@ -162,6 +286,7 @@ void TestUTF8(void)
 	TestUTF8Example1();
 	TestUTF8Example2();
 	TestUTF8Example3();
+	TestUTF8Example4();
 
 	TestStatistics();
 }
