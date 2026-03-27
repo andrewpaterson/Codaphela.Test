7d66f4e8ded0c68456523e61bb696f646ba83985
Andrew Paterson <andrew.ian.paterson@gmail.com>
2026-03-27 17:49:48 +0200

    Fixed a bunch of stuff in IndexObject.

--- TestStandardLib/TestStandardLib/TestIndexObject.cpp
+++ TestStandardLib/TestStandardLib/TestIndexObject.cpp
@@ -13,12 +13,166 @@
 //////////////////////////////////////////////////////////////////////////
 void TestIndexObjectPut(void)
 {
-	Ptr<CRoot>					pRoot;
-	
 	ObjectsInit();
 
-	pRoot = ORoot();
+	{
+		Ptr<CRoot>					pRoot;
+		Ptr<CIndexObject>			pIndex;
+		Ptr<CTestObject>			pTest1;
+		Ptr<CTestObject>			pTest2;
+		Ptr<CTestObject>			pTest3;
+		STestObjectFreedNotifier	sNotifier1;
+		STestObjectFreedNotifier	sNotifier2;
+		STestObjectFreedNotifier	sNotifier3;
 
+		pRoot = ORoot();
+		pIndex = OMalloc<CIndexObject>();
+		pRoot->Add(pIndex);
+
+		pIndex->Put("shnork", OMalloc<CTestObject>(&sNotifier1));
+		pIndex->Put("shnerp", OMalloc<CTestObject>(&sNotifier2));
+		pIndex->Put("booger", OMalloc<CTestObject>(&sNotifier3));
+
+		AssertSize(3, pIndex->NumElements());
+		AssertSize(6, gcObjects.NumMemoryIndexes());
+
+		pIndex = NULL;
+		AssertSize(6, gcObjects.NumMemoryIndexes());
+
+		pIndex = pRoot->GetFirst<CIndexObject>();
+		pTest1 = pIndex->Get("booger");
+		AssertPointer(&sNotifier3, pTest1->mpsFreedNotifier);
+		pTest2 = pIndex->Get("shnerp");
+		AssertPointer(&sNotifier2, pTest2->mpsFreedNotifier);
+		pTest3 = pIndex->Get("shnork");
+		AssertPointer(&sNotifier1, pTest3->mpsFreedNotifier);
+	}
+
+	ObjectsFlush();
+	ObjectsKill();
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestIndexObjectDetach(void)
+{
+	ObjectsInit();
+
+	{
+		Ptr<CRoot>					pRoot;
+		Ptr<CIndexObject>			pIndex;
+		Ptr<CTestObject>			pTest1;
+		Ptr<CTestObject>			pTest2;
+		Ptr<CTestObject>			pTest3;
+		STestObjectFreedNotifier	sNotifier1;
+		STestObjectFreedNotifier	sNotifier2;
+		STestObjectFreedNotifier	sNotifier3;
+
+		pRoot = ORoot();
+		pIndex = OMalloc<CIndexObject>();
+		pRoot->Add(pIndex);
+
+		pIndex->Put("shnork", OMalloc<CTestObject>(&sNotifier1));
+		pIndex->Put("shnerp", OMalloc<CTestObject>(&sNotifier2));
+		pIndex->Put("booger", OMalloc<CTestObject>(&sNotifier3));
+		AssertFalse(sNotifier1.bFreed);
+		AssertFalse(sNotifier2.bFreed);
+		AssertFalse(sNotifier3.bFreed);
+
+		AssertSize(3, pIndex->NumElements());
+		AssertSize(6, gcObjects.NumMemoryIndexes());
+
+
+		pRoot->RemoveAll();
+		pIndex = NULL;
+		AssertSize(2, gcObjects.NumMemoryIndexes());
+		AssertTrue(sNotifier1.bFreed);
+		AssertTrue(sNotifier2.bFreed);
+		AssertTrue(sNotifier3.bFreed);
+	}
+
+	ObjectsFlush();
+	ObjectsKill();
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
+void TestIndexObjectIterateNull(void)
+{
+	ObjectsInit();
+
+	{
+		Ptr<CRoot>					pRoot;
+		Ptr<CIndexObject>			pIndex;
+		Ptr<CTestObject>			pTest1;
+		Ptr<CTestObject>			pTest2;
+		Ptr<CTestObject>			pTest3;
+		STestObjectFreedNotifier	sNotifier1;
+		STestObjectFreedNotifier	sNotifier2;
+		STestObjectFreedNotifier	sNotifier3;
+		SIndexTreeMemoryIterator	sIter;
+		uint8						auiKey[8];
+		size						uiKeySize;
+		bool						bExists;
+		CPointer					pPtr;
+
+		pRoot = ORoot();
+		pIndex = OMalloc<CIndexObject>();
+		pRoot->Add(pIndex);
+
+		pTest3 = OMalloc<CTestObject>(&sNotifier1);
+		pTest2 = OMalloc<CTestObject>(&sNotifier2);
+		pTest1 = OMalloc<CTestObject>(&sNotifier3);
+		pIndex->Put("shnork", pTest3);
+		pIndex->Put("shniqu", Null<CObject>());
+		pIndex->Put("shnerp", pTest2);
+		pIndex->Put("booger", pTest1);
+		pIndex->Put("bozzul", Null<CObject>());
+
+		AssertSize(5, pIndex->NumElements());
+		AssertSize(6, gcObjects.NumMemoryIndexes());
+
+		memset(auiKey, 0, 8);
+		pPtr = pIndex->StartIteration(&sIter, auiKey, &uiKeySize, 8, &bExists);
+		AssertTrue(bExists);
+		AssertString("booger", auiKey);
+		AssertPointer(&pTest1, &pPtr);
+
+		memset(auiKey, 0, 8);
+		pPtr = pIndex->Iterate(&sIter, auiKey, &uiKeySize, 8, &bExists);
+		AssertTrue(bExists);
+		AssertString("bozzul", auiKey);
+		AssertNull(&pPtr);
+
+		memset(auiKey, 0, 8);
+		pPtr = pIndex->Iterate(&sIter, auiKey, &uiKeySize, 8, &bExists);
+		AssertTrue(bExists);
+		AssertString("shnerp", auiKey);
+		AssertPointer(&pTest2, &pPtr);
+
+		memset(auiKey, 0, 8);
+		pPtr = pIndex->Iterate(&sIter, auiKey, &uiKeySize, 8, &bExists);
+		AssertTrue(bExists);
+		AssertString("shniqu", auiKey);
+		AssertNull(&pPtr);
+
+		memset(auiKey, 0, 8);
+		pPtr = pIndex->Iterate(&sIter, auiKey, &uiKeySize, 8, &bExists);
+		AssertTrue(bExists);
+		AssertString("shnork", auiKey);
+		AssertPointer(&pTest3, &pPtr);
+
+		memset(auiKey, 0, 8);
+		pPtr = pIndex->Iterate(&sIter, auiKey, &uiKeySize, 8, &bExists);
+		AssertFalse(bExists);
+		AssertNull(&pPtr);
+	}
 
 	ObjectsFlush();
 	ObjectsKill();
@@ -36,7 +190,9 @@ void TestIndexObject(void)
 	TypesInit();
 	DataIOInit();
 
-	TestIndexObjectPut();
+	//TestIndexObjectPut();
+	//TestIndexObjectDetach();
+	TestIndexObjectIterateNull();
 
 	DataIOKill();
 	TypesKill();
