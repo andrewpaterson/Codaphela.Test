ef06ac0a977f36fc0c7c9d205beab7fec7208284
Andrew Paterson <andrew.ian.paterson@gmail.com>
2026-05-17 18:49:29 +0200

    Write a whole cel layer to image.

--- TestSupportLib/TestSupportLib/TestMaps.cpp
+++ TestSupportLib/TestSupportLib/TestMaps.cpp
@@ -4,6 +4,8 @@
 #include "SupportLib/ImageWriter.h"
 #include "SupportLib/ImageDivider.h"
 #include "SupportLib/Maps.h"
+#include "SupportLib/TileLayerCel.h"
+#include "SupportLib/ImageWriter.h"
 #include "TestLib/Assert.h"
 #include "SupportAssert.h"
 
@@ -14,6 +16,13 @@
 //////////////////////////////////////////////////////////////////////////
 void TestMapsGenerate(void)
 {
+	char				szDirectory[] = "Output" _FS_ "MapsGenerate";
+	CIndexTreeMemory	cMemory;
+	CFileUtil			cFileUtil;
+
+	AssertTrue(cFileUtil.RemoveDir(szDirectory));
+	AssertTrue(cFileUtil.TouchDir(szDirectory));
+
 	DataIOInit();
 	ObjectsInit();
 	{
@@ -21,21 +30,30 @@ void TestMapsGenerate(void)
 		CImageDivider			cImageDivider;
 		CImageDividerNumbers	cNumbers;
 		Ptr<CArrayImageCel>		pacBackgroundCels;
-		CFileUtil				cFileUtil;
-		CChars					szFilename;
+		CChars					szInputFilename;
+		CChars					szOutputFilename;
+		Ptr<CImage>				pBackground;
+		Ptr<CTileMap>			pTileMap;
+		Ptr<CTileLayerCel>		pCelLayer;
+		Ptr<CImageCel>			pCel;
+		CSubImage*				pcSubImage;
+		bool					b1, b2, b3, b4;
+		Ptr<CImage>				pWholeMap;
+		bool					bWritten;
+		size					uiTile;
 
 		ImageChannelDescriptorInit();
 
-		szFilename.Init();
-		cFileUtil.CurrentDirectory(&szFilename);
-		cFileUtil.AppendToPath(&szFilename, "Input");
-		cFileUtil.AppendToPath(&szFilename, "SpaceCrusade");
-		cFileUtil.AppendToPath(&szFilename, "Tiles.png");
-		AssertTrue(cFileUtil.Exists(szFilename.Text()));
-		szFilename.Kill();
+		szInputFilename.Init();
+		cFileUtil.CurrentDirectory(&szInputFilename);
+		cFileUtil.AppendToPath(&szInputFilename, "Input");
+		cFileUtil.AppendToPath(&szInputFilename, "SpaceCrusade");
+		cFileUtil.AppendToPath(&szInputFilename, "Tiles.png");
+		AssertTrue(cFileUtil.Exists(szInputFilename.Text()));
 
-		Ptr<CImage> pBackground = ReadImage("Input" _FS_ "SpaceCrusade" _FS_ "Tiles.png", IT_Unknown, true);
+		pBackground = ReadImage(szInputFilename.Text(), IT_Unknown, true);
 		AssertTrue(pBackground.IsNotNull());
+		szInputFilename.Kill();
 
 		cNumbers.InitGeneral(-1, -1, 10, 3, 0, 0, 0, 0);
 		cImageDivider.Init(&pBackground, NULL);
@@ -43,8 +61,40 @@ void TestMapsGenerate(void)
 		pacBackgroundCels = cImageDivider.GetDestImageCels();
 		AssertSize(30, pacBackgroundCels->NumElements());
 
+		pCel = pacBackgroundCels->Get(0);
+		pcSubImage = pCel->GetSubImage();
+		AssertSize(16, pcSubImage->GetFullWidth());
+		AssertSize(16, pcSubImage->GetFullHeight());
+
+		for (uiTile = 0; uiTile < 30; uiTile++)
+		{
+			pCel = pacBackgroundCels->Get(uiTile);
+			AssertTrue(pCel.IsNotNull());
+		}
+
 		cMaps.Init();
-		//Do something here.
+		
+		pTileMap = OMalloc<CTileMap>();
+		cMaps.AddMap(pTileMap);
+		pCelLayer = OMalloc<CTileLayerCel>(pTileMap, "Cel", SInt32Vec2(4, 4), SInt32Vec2(16, 16), SInt32Vec2(0, 0));
+		b1 = pCelLayer->SetTiles(0, 0, pacBackgroundCels,  7, 5, 5, 6);
+		b2 = pCelLayer->SetTiles(0, 1, pacBackgroundCels, 19, 2, 2, 4);
+		b3 = pCelLayer->SetTiles(0, 2, pacBackgroundCels, 18, 2, 3, 4);
+		b4 = pCelLayer->SetTiles(0, 3, pacBackgroundCels,  9, 5, 5, 8);
+		AssertTrue(b1);
+		AssertTrue(b2);
+		AssertTrue(b3);
+		AssertTrue(b4);
+
+		szOutputFilename.Init(szDirectory);
+		cFileUtil.AppendToPath(&szOutputFilename, "TestTileLayer.png");
+
+		pWholeMap = pCelLayer->WriteToImage();
+		AssertTrue(pWholeMap.IsNotNull());
+
+		bWritten = WriteImage(pWholeMap, szOutputFilename.Text(), IT_PNG);
+		AssertTrue(bWritten);
+
 		cMaps.Kill();
 
 		ImageChannelDescriptorKill();
