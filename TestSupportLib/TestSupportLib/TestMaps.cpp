d0db9ec2171d0c26ad4665bf304ba11731466656
Andrew Paterson <andrew.ian.paterson@gmail.com>
2026-05-21 19:34:35 +0200

    Tried fixing the image copier.  Turned out to be the GDI png writer instead.

--- TestSupportLib/TestSupportLib/TestMaps.cpp
+++ TestSupportLib/TestSupportLib/TestMaps.cpp
@@ -72,6 +72,99 @@ Ptr<CArrayImageCel> TestMapsReadSpaceCrusadeCels(char* szFilename, int iColumnCo
 //
 //
 //////////////////////////////////////////////////////////////////////////
+void TestMapsTileMap(void)
+{
+	char				szDirectory[] = "Output" _FS_ "MapsGenerate";
+	CIndexTreeMemory	cMemory;
+	CFileUtil			cFileUtil;
+
+	AssertTrue(cFileUtil.RemoveDir(szDirectory));
+	AssertTrue(cFileUtil.TouchDir(szDirectory));
+
+	DataIOInit();
+	ObjectsInit();
+	{
+		CMaps					cMaps;
+		CImageDivider			cImageDivider;
+		CImageDividerNumbers	cNumbers;
+		Ptr<CArrayImageCel>		pacBackgroundCels;
+		CChars					szInputFilename;
+		CChars					szOutputFilename;
+		Ptr<CImage>				pBackground;
+		Ptr<CTileMap>			pTileMap;
+		Ptr<CTileLayerCel>		pCelLayer;
+		Ptr<CImageCel>			pCel;
+		CSubImage*				pcSubImage;
+		bool					b1, b2, b3, b4;
+		Ptr<CImage>				pWholeMap;
+		bool					bWritten;
+		size					uiTile;
+
+		ImageChannelDescriptorInit();
+
+		pBackground = TestMapsReadSpaceCrusadeImage("Tiles.png");
+
+		cNumbers.InitGeneral(-1, -1, 10, 3, 0, 0, 0, 0);
+		cImageDivider.Init(&pBackground, NULL);
+		cImageDivider.GenerateFromNumbers(&cNumbers);
+		pacBackgroundCels = cImageDivider.GetDestImageCels();
+		AssertSize(30, pacBackgroundCels->NumElements());
+
+		pCel = pacBackgroundCels->Get(0);
+		pcSubImage = pCel->GetSubImage();
+		AssertSize(16, pcSubImage->GetFullWidth());
+		AssertSize(16, pcSubImage->GetFullHeight());
+
+		for (uiTile = 0; uiTile < 30; uiTile++)
+		{
+			pCel = pacBackgroundCels->Get(uiTile);
+			AssertTrue(pCel.IsNotNull());
+		}
+
+		cMaps.Init();
+		
+		pTileMap = OMalloc<CTileMap>();
+		cMaps.AddMap(pTileMap);
+		pCelLayer = OMalloc<CTileLayerCel>(pTileMap, "Cel", SInt32Vec2(4, 4), SInt32Vec2(16, 16), SInt32Vec2(0, 0));
+		b1 = pCelLayer->SetTiles(0, 0, pacBackgroundCels,  7, 5, 5, 6);
+		b2 = pCelLayer->SetTiles(0, 1, pacBackgroundCels, 19, 2, 2, 4);
+		b3 = pCelLayer->SetTiles(0, 2, pacBackgroundCels, 18, 2, 3, 4);
+		b4 = pCelLayer->SetTiles(0, 3, pacBackgroundCels,  9, 5, 5, 8);
+		AssertTrue(b1);
+		AssertTrue(b2);
+		AssertTrue(b3);
+		AssertTrue(b4);
+
+		szOutputFilename.Init(szDirectory);
+		cFileUtil.AppendToPath(&szOutputFilename, "TestTileLayer.png");
+
+		pWholeMap = pCelLayer->WriteToImage();
+		AssertTrue(pWholeMap.IsNotNull());
+
+		bWritten = WriteImage(pWholeMap, szOutputFilename.Text(), IT_PNG);
+		AssertTrue(bWritten);
+
+		cMaps.Kill();
+
+		ImageChannelDescriptorKill();
+
+		szInputFilename.Init(szOutputFilename);
+		szInputFilename.Replace("Output", "Input");
+
+		AssertFile(szInputFilename, szOutputFilename);
+
+		szOutputFilename.Kill();
+		szInputFilename.Kill();
+	}
+	ObjectsKill();
+	DataIOKill();
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+//
+//
+//////////////////////////////////////////////////////////////////////////
 void TestMapsSpriteMap(void)
 {
 	char				szDirectory[] = "Output" _FS_ "MapsSpriteMap";
@@ -97,8 +190,8 @@ void TestMapsSpriteMap(void)
 		Ptr<CImageCel>			pCel3;
 		Ptr<CImageCel>			pCel4;
 		Ptr<CImageCel>			pCel5;
-		Ptr<CImageCel>			pCel6 ;
-		CSubImage*				pcSubImage;
+		Ptr<CImageCel>			pCel6;
+		CSubImage* pcSubImage;
 		Ptr<CImage>				pWholeMap;
 		bool					bWritten;
 		size					uiSprite;
@@ -176,119 +269,25 @@ void TestMapsSpriteMap(void)
 		AssertNotPointer(&pCel5->GetSourceImage(), &pCel1->GetSourceImage());
 
 		cMaps.Init();
-		
+
 		pSpriteMap = OMalloc<CSpriteMap>();
 		cMaps.AddMap(pSpriteMap);
 
-		pSpriteMap->AddSprite(OMalloc<CSprite>(pCel1, 18, 29)); 
-		pSpriteMap->AddSprite(OMalloc<CSprite>(pCel2,  2, 36));
-		pSpriteMap->AddSprite(OMalloc<CSprite>(pCel3, 31,  9));
+		pSpriteMap->AddSprite(OMalloc<CSprite>(pCel1, 18, 29));
+		pSpriteMap->AddSprite(OMalloc<CSprite>(pCel2, 2, 36));
+		pSpriteMap->AddSprite(OMalloc<CSprite>(pCel3, 31, 9));
 		pSpriteMap->AddSprite(OMalloc<CSprite>(pCel4, 39, 27));
-		pSpriteMap->AddSprite(OMalloc<CSprite>(pCel5, 11,  3));
+		pSpriteMap->AddSprite(OMalloc<CSprite>(pCel5, 11, 3));
 		pSpriteMap->AddSprite(OMalloc<CSprite>(pCel6, 25, 19));
 
 		AssertSize(6, pSpriteMap->NumSprites());
 
-		szOutputFilename.Init(szDirectory);
-		cFileUtil.AppendToPath(&szOutputFilename, "TestSpriteMap.png");
-
 		pWholeMap = pSpriteMap->WriteToImage();
 		AssertTrue(pWholeMap.IsNotNull());
 
-		bWritten = WriteImage(pWholeMap, szOutputFilename.Text(), IT_PNG);
-		AssertTrue(bWritten);
-
-		cMaps.Kill();
-
-		ImageChannelDescriptorKill();
-
-		szInputFilename.Init(szOutputFilename);
-		szInputFilename.Replace("Output", "Input");
-
-		AssertFile(szInputFilename, szOutputFilename);
-
-		szOutputFilename.Kill();
-		szInputFilename.Kill();
-	}
-	ObjectsKill();
-	DataIOKill();
-}
-
-
-//////////////////////////////////////////////////////////////////////////
-//
-//
-//////////////////////////////////////////////////////////////////////////
-void TestMapsTileMap(void)
-{
-	char				szDirectory[] = "Output" _FS_ "MapsGenerate";
-	CIndexTreeMemory	cMemory;
-	CFileUtil			cFileUtil;
-
-	AssertTrue(cFileUtil.RemoveDir(szDirectory));
-	AssertTrue(cFileUtil.TouchDir(szDirectory));
-
-	DataIOInit();
-	ObjectsInit();
-	{
-		CMaps					cMaps;
-		CImageDivider			cImageDivider;
-		CImageDividerNumbers	cNumbers;
-		Ptr<CArrayImageCel>		pacBackgroundCels;
-		CChars					szInputFilename;
-		CChars					szOutputFilename;
-		Ptr<CImage>				pBackground;
-		Ptr<CTileMap>			pTileMap;
-		Ptr<CTileLayerCel>		pCelLayer;
-		Ptr<CImageCel>			pCel;
-		CSubImage*				pcSubImage;
-		bool					b1, b2, b3, b4;
-		Ptr<CImage>				pWholeMap;
-		bool					bWritten;
-		size					uiTile;
-
-		ImageChannelDescriptorInit();
-
-		pBackground = TestMapsReadSpaceCrusadeImage("Tiles.png");
-
-		cNumbers.InitGeneral(-1, -1, 10, 3, 0, 0, 0, 0);
-		cImageDivider.Init(&pBackground, NULL);
-		cImageDivider.GenerateFromNumbers(&cNumbers);
-		pacBackgroundCels = cImageDivider.GetDestImageCels();
-		AssertSize(30, pacBackgroundCels->NumElements());
-
-		pCel = pacBackgroundCels->Get(0);
-		pcSubImage = pCel->GetSubImage();
-		AssertSize(16, pcSubImage->GetFullWidth());
-		AssertSize(16, pcSubImage->GetFullHeight());
-
-		for (uiTile = 0; uiTile < 30; uiTile++)
-		{
-			pCel = pacBackgroundCels->Get(uiTile);
-			AssertTrue(pCel.IsNotNull());
-		}
-
-		cMaps.Init();
-		
-		pTileMap = OMalloc<CTileMap>();
-		cMaps.AddMap(pTileMap);
-		pCelLayer = OMalloc<CTileLayerCel>(pTileMap, "Cel", SInt32Vec2(4, 4), SInt32Vec2(16, 16), SInt32Vec2(0, 0));
-		b1 = pCelLayer->SetTiles(0, 0, pacBackgroundCels,  7, 5, 5, 6);
-		b2 = pCelLayer->SetTiles(0, 1, pacBackgroundCels, 19, 2, 2, 4);
-		b3 = pCelLayer->SetTiles(0, 2, pacBackgroundCels, 18, 2, 3, 4);
-		b4 = pCelLayer->SetTiles(0, 3, pacBackgroundCels,  9, 5, 5, 8);
-		AssertTrue(b1);
-		AssertTrue(b2);
-		AssertTrue(b3);
-		AssertTrue(b4);
-
 		szOutputFilename.Init(szDirectory);
-		cFileUtil.AppendToPath(&szOutputFilename, "TestTileLayer.png");
-
-		pWholeMap = pCelLayer->WriteToImage();
-		AssertTrue(pWholeMap.IsNotNull());
-
-		bWritten = WriteImage(pWholeMap, szOutputFilename.Text(), IT_PNG);
+		cFileUtil.AppendToPath(&szOutputFilename, "TestSpriteMap.png");
+		bWritten = WriteImage(pWholeMap, szOutputFilename.Text());
 		AssertTrue(bWritten);
 
 		cMaps.Kill();
